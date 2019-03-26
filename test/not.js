7e06247f0b4d86b030da48f7c3ec6a50c4c0ff52
Filipe Vieira <sv.filipe@gmail.com>
2019-03-26 23:48:29 +0000

    clean up

--- test/not.js
+++ test/not.js
@@ -281,7 +281,7 @@ describe("Not Tests.", () => {
 		)
 	);
 
-	it("Should declare a number Set, 4 elements",
+	xit("Should declare a number Set, 4 elements",
 		test(
 			`(number 0)
             (number 1)
